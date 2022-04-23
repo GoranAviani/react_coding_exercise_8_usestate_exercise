5d0e04020c1c4348147e6597d2ca1ef9abc3e8ae
GoranAviani <goranaviani@gmail.com>
2022-04-23 13:35:32 +0200

    Coding Exercise 8: useState Exercise

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 
 const App = () => {
+    const [counter, setCounter] = useState(0)
 
     const onButtonClick = () => {
 
@@ -11,7 +12,7 @@ const App = () => {
         <div>
             App
             <button onClick={onButtonClick}>Button</button>
-            <h1>Current count: </h1>
+            <h1>Current count: {counter}</h1>
         </div>
     )
 }
