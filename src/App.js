75ee617d531731630222bfb92aafb606f12cf9ce
GoranAviani <goranaviani@gmail.com>
2022-04-23 13:34:13 +0200

    Coding Exercise 8: useState Exercise

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, {useState} from "react";
 
 const App = () => {
+
+
     return (
         <div>
             App
+            <button onClick={console.log('click')}>Button</button>
         </div>
     )
 }
